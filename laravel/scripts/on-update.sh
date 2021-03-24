10cb9cc4287f51ed6d1f9f145ee7393041fd0ba4
Henrique Rangel <hrangel@nucleo235.com.br>
2021-03-23 22:27:41 -0300

    Add cghooks inside script (even if it fails)

--- laravel/scripts/on-update.sh
+++ laravel/scripts/on-update.sh
@@ -8,8 +8,11 @@ echo "  DEPLOY_ENV: $DEPLOY_ENV"  >> $log_file
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "  SCRIPTS_DIR: $SCRIPTS_DIR"  >> $log_file
 
+echo "/   ON-UPDATE: CGHOOKS" >> $log_file
+cghooks update >> $log_file
+
 if [ "$DEPLOY_ENV" = "production" ]; then
-  echo "   ON-UPDATE: BASH" >> $log_file
+  echo "/   ON-UPDATE: BASH" >> $log_file
   source ~/.bash_profile
 
   echo "   ON-UPDATE: migrate" >> $log_file
