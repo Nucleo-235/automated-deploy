c865cfffd71b3fc0c96a8149b086c78678198f13
Henrique Rangel <hrangel@nucleo235.com.br>
2021-03-28 10:46:40 -0300

    add laravel on install script to cover cghoos

--- laravel/scripts/add-composer-script.php
+++ laravel/scripts/add-composer-script.php
@@ -3,18 +3,42 @@
 $composerConfig = json_decode(file_get_contents('composer.json'), true);
 
 if (!array_key_exists('scripts', $composerConfig)) {
-  $composerConfig['scripts'] = [];
+    $composerConfig['scripts'] = [];
 }
 
+// INSTALL
+if (!array_key_exists('post-install-cmd', $composerConfig['scripts'])) {
+    $composerConfig['post-install-cmd'] = [];
+}
+
+// Make the cghooks stop throwing errors
+$command = 'cghooks add --ignore-lock';
+$index = array_search($command, $composerConfig['scripts']['post-install-cmd'], false);
+if (false !== $index) {
+    unset($composerConfig['scripts']['post-install-cmd'][(int)$index]);
+}
+
+// Add the deploy script
+$command = 'export $(cat .env | xargs) && . "$PWD/deploy/laravel/scripts/on-install.sh"';
+$index = array_search($command, $composerConfig['scripts']['post-install-cmd'], false);
+if (false === $index) {
+    if (count($composerConfig['scripts']['post-install-cmd']) == 0) {
+        $composerConfig['scripts']['post-install-cmd'] = [ $command ];
+    } else {
+        array_push($composerConfig['scripts']['post-install-cmd'], $command);
+    }
+}
+
+// UPDATE
 if (!array_key_exists('post-update-cmd', $composerConfig['scripts'])) {
-  $composerConfig['post-update-cmd'] = [];
+    $composerConfig['post-update-cmd'] = [];
 }
 
 // Make the cghooks stop throwing errors
 $command = 'cghooks update';
 $index = array_search($command, $composerConfig['scripts']['post-update-cmd'], false);
 if (false !== $index) {
-  unset($composerConfig['scripts']['post-update-cmd'][(int)$index]);
+    unset($composerConfig['scripts']['post-update-cmd'][(int)$index]);
 }
 
 // Add the deploy script
